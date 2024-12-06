ba4e5a1a0b9c2becaf7afc35fda9c87e7ea32088
0sugo <josugo26@gmail.com>
2024-12-06 09:44:19 +0300

    setup inital layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,169 @@
+"use client";
+import * as React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { cn } from "@/lib/utils";
+import logo from "@/app/images/logo.svg";
+import hero from "@/app/images/hero.png";
+import { FaAngleDoubleDown, FaArrowDown, FaArrowRight, FaRegLightbulb } from "react-icons/fa";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport, navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu"
+const components = [
+  {
+    title: "Home",
+    href: "/",
+    description:
+      "Home page",
+  },
+  {
+    title: "Treatment cartegories",
+    href: "/treatment cartegories",
+    description:
+      "Treatment cartegories",
+  },
+  {
+    title: "Patient Journey",
+    href: "/patient journey",
+    description:
+      "Patient Journey.",
+  },
+  {
+    title: "About",
+    href: "/about",
+    description: "About page.",
+  },
+  {
+    title: "Contact us",
+    href: "/contact us",
+    description: "Contact us.",
+  }
+];
+
+export function NavigationBarMd() {
+  return (
+    <nav className="">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {components.map((component, index) => (
+            <NavigationMenuItem key={component.href}  >
+              <Link href={component.href} passHref >
+                <NavigationMenuLink className={`${navigationMenuTriggerStyle()} flex-nowrap bg-inherit text-[#170700] hover:bg-inherit hover:underline hover:text-[#2c2222]`}>
+                  {component.title}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+
+    </nav>
+
+  )
+};
+
+export function NavigationBarSm() {
+  return (
+    <nav>
+      <NavigationMenu>
+        <NavigationMenuTrigger>
+          <button className="p-2">
+            <Image
+              src="/images/menu.svg"
+              alt="Menu"
+              width={24}
+              height={24}
+            />
+          </button>
+        </NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuViewport>
+            <NavigationMenuList>
+              {components.map((component) => (
+                <NavigationMenuItem key={component.href}>
+                  <Link href={component.href} passHref>
+                    <NavigationMenuLink>
+                      {component.title}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+            <NavigationMenuIndicator />
+          </NavigationMenuViewport>
+        </NavigationMenuContent>
+      </NavigationMenu>
+    </nav>
+  )
+}
+
+export function HeroSection() {
+  return (
+    <section className="w-[90%]">
+      <div className="grid grid-cols-2">
+
+        <div className="flex flex-col justify-center px-12">
+          <h1 className="text-4xl md:text-5xl font-bold flex flex-col gap-4">Empowering Reproductive Care Every Step of the Way</h1>
+
+          <div className="mt-10 text-lg">
+            <p className="">Explore personalized care and expert support for your sexual & reproductive health, with guidance you can trust and compassion every step of the way.</p>
+            <div className="flex gap-2">
+              <button className="bg-[#6b52b5] text-white text-sm rounded-full p-3 mt-10 flex items-center gap-4">Book appointment<span className="bg-white rounded-full text-[#6b52b5] p-2"><FaArrowRight /></span></button>
+              <button className="text-[#6b52b5] bg-white text-sm rounded-full p-3 mt-10 flex items-center gap-4 border-2 border-[#DBCF8]"><span className="text-white rounded-full bg-[#6b52b5] p-2"><FaRegLightbulb /></span>Learn more</button>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="flex justify-end ">
+          <div className="border-2 p-2 border-[#ff788e] rounded-full">
+            <div className="bg-[#9674ff] p-8 rounded-full flex justify-end ">
+              <Image src={hero} alt="Hero" width={400} height={400} className="rounded-full" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center relative  "><FaAngleDoubleDown className="absolute size-7 text-[#7428ea] animate-bounce" /></div>
+    </section>
+  )
+}
+
+export function PatientJourneySection() {
+  return(
+    <section>
+
+    </section>
+  );
+
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="min-h-screen flex flex-col items-center gap-4">
+      <header className="flex pt-10 w-[90%] justify-between items-center">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex items-center gap-16">
+          <Image src={logo} alt="Logo" className="h-20 w-20 bg-white rounded-full p-1" />
+          <NavigationBarMd className="text-xl" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div>
+          <span className="bg-[#6b52b5] font-medium text-white rounded-3xl text-sm p-3">Book appointment</span>
+        </div>
+      </header>
+
+      <HeroSection />
+
+
+
+    </main>
   );
 }
